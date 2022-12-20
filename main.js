c03250621a43c731f0030bfe7dd617732cc53ecc
Nelson Carvalho <nelsonacf@yahoo.com>
2022-12-20 07:00:19 -0300

    modificado codigo js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const form = document.getElementById('form-deposito');
+const nomeBeneficiario = document.getElementById('nome-beneficiario');
 
 function validaNome(nomeCompleto) {
     const nomeComoArray = nomeCompleto.split(' ');
@@ -9,7 +10,7 @@ form.addEventListener('submit', function (e) {
     let formEValido = false;
     e.preventDefault();
 
-    const nomeBeneficiario = document.getElementById('nome-beneficiario');
+    
     const numeroContaBeneficiario = document.getElementById('numero-conta');
     const valorDeposito = document.getElementById('valor-deposito');
     const mensagemSucesso = `Montante de: <b>${valorDeposito.value}</b> 
@@ -24,8 +25,20 @@ form.addEventListener('submit', function (e) {
         numeroContaBeneficiario.value = '';
         valorDeposito.value = '';
     } else {
-        alert("O nome não está completo");
+        nomeBeneficiario.style.border = '1px solid red'
+        document.querySelector('.error-message').style.display = 'block';
     }
 })
 
-console.log(form);
+nomeBeneficiario.addEventListener('keyup', function (e) {
+    console.log(e.target.value);
+    formEValido = validaNome(e.target.value);
+    if (!formEValido) {       
+        nomeBeneficiario.classList.add('error');
+        document.querySelector('.error-message').style.display = 'block';
+    } else {
+        nomeBeneficiario.classList.remove('error');
+        document.querySelector('.error-message').style.display = 'none'; 
+    }     
+
+});
